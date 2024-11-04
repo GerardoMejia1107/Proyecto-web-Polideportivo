b7b4847f704a31d24314f3a90b9b76ec3fe0f8d4
GerardoMejia1107 <00104923@uca.edu.sv>
2024-11-03 18:19:28 -0600

    [Proyecto] Add: NEstilo de la pagina principal del administrador

--- Frontend/Polideportivo/src/App.jsx
+++ Frontend/Polideportivo/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './styles/App.css'
 import './styles/index.css'
 import Home from "./pages/Usuario/Home.jsx";
+import HomeDashboard from "./pages/Admin/HomeDashboard/HomeDashboard.jsx";
 
 function App() {
   return (
     <div className='global-container'>
-    <Home/>
+    <HomeDashboard/>
     </div>
   )
 }
---
+++ Frontend/Polideportivo/src/pages/Admin/HomeDashboard/HomeDashboard.jsx
@@ -0,0 +1,29 @@
+import HeaderAdmin from "../../../components/Header/HeaderAdmin.jsx";
+import UcaLogo from "../../../assets/images/UCA.png";
+import './HomeDashboard.css'
+
+const HomeDashboard = ()=>{
+    return (
+        <section className={'content'}>
+            <HeaderAdmin/>
+            <main className={'container'}>
+                <div className={'main-content'}>
+                    <div className={'container flex'}>
+                        <img className={'logo-uca'} src={UcaLogo} alt={'Logo Uca'}/>
+                        <h1 className={'title-uca'}>Universidad Centroamericana José Simón Cañas</h1>
+                    </div>
+
+                    <div className={'data-from-db matches'}>
+                        <h2>Partidos transcurriendo</h2>
+                    </div>
+                    <div className={'data-from-db trainings'}>
+                        <h2>Entrenamientos transcurriendo</h2>
+                    </div>
+
+                </div>
+            </main>
+        </section>
+    );
+}
+
+export default HomeDashboard;
