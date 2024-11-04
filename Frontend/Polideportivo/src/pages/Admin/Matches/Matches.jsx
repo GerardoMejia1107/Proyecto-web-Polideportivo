cda2ccd26f31a42ad0cea47abc6e9fad59057b65
GerardoMejia1107 <00104923@uca.edu.sv>
2024-11-03 23:46:16 -0600

    [Proyecto] Add: Diseño de la tabla en Partidos

--- Frontend/Polideportivo/src/pages/Admin/Matches/Matches.jsx
+++ Frontend/Polideportivo/src/pages/Admin/Matches/Matches.jsx
@@ -1,10 +1,17 @@
-import NavAdmin from "../../../components/Admin/NavAdmin/NavAdmin.jsx";
+
 import HeaderAdmin from "../../../components/Admin/HeaderAdmin/HeaderAdmin.jsx";
+import TableData from "../../../components/Admin/Table/TableData.jsx";
+import './Matches.css'
+
+
 
 const Matches = ()=>{
     return (
         <section className={'content'}>
             <HeaderAdmin/>
+            <div className="table-container">
+                <TableData/>
+            </div>
         </section>
     )
 }
