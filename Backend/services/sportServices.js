fc5549eac1298a6dbc663d29ab4818e175bfec77
Miguel Mejia <00104923@uca.edu.sv>
2024-11-05 09:38:43 -0600

    [Proyecto] Add: Pruebas de conexion de la BD, se modificaron los servicios de la coleccion Sport

--- Backend/services/sportServices.js
+++ Backend/services/sportServices.js
@@ -2,30 +2,38 @@ import Sport from "../models/sportModel.js";
 
 export const createSport = async (sportData) => {
   try {
-    //REVIEW - Expression regex para verificar los nombrse de los deportes, independientemente de como sea escrito
-    const regex = new RegExp(`^${sportData.name.trim()}$`, "i");
-    const existingSport = await Sport.findOne({ name: { $regex: regex } });
-    if (!existingSport) {
+    //Verifico que no exista el deporte
+    if (!(await checkSportExists(sportData))) {
       const newSport = new Sport(sportData);
       return await newSport.save();
     }
     throw new Error("Sport already exists");
   } catch (e) {
-    throw new Error(e);
+    throw new Error(e.message);
+  }
+};
+
+const checkSportExists = async (sportData) => {
+  try {
+    //Expression regex para encontrar si el nombre del deporte existe, independiente mente del patron del nombre
+    const regex = new RegExp(`^${sportData.name.trim()}$`, "i"); //Le paso el nombre que contiene sportData
+    const existingSport = await Sport.findOne({ name: { $regex: regex } }); //Busco en la BD si existe algun documento con el patron
+    return !!existingSport; //Convierto el paso anterior en un boolean, si existe retornara true, sino entonces retorna false
+  } catch (e) {
+    throw new Error(e.message);
   }
 };
 
 export const getAllSports = async () => {
   try {
-    const sports = Sport.find();
-    return await sports;
+    return await Sport.find(); //Retorno todos los documentos de la BD de la coleccion Sport
   } catch (err) {
-    throw new Error(err);
+    throw new Error(err.message);
   }
 };
 
 export const getMySportById = async (id) => {
-  const sport = await Sport.findById(id);
+  const sport = await Sport.findById(id); //Hago la busqueda del doc con el id que le pase
   if (!sport) {
     throw new Error("Sport not found");
   }
@@ -33,22 +41,30 @@ export const getMySportById = async (id) => {
 };
 
 export const deleteSportById = async (id) => {
-  const sport = await getMySportById(id);
-  if (sport) {
-    await sport.deleteOne();
-    return sport;
+  try {
+    const sport = await getMySportById(id); //Verifico la existencia del doc con el id especificado
+    if (sport) {
+      await sport.deleteOne();
+      return { message: "Sport deleted successfully", sport };
+    }
+  } catch (e) {
+    throw new Error(e.message);
   }
 };
 
 export const updateSportById = async (id, updatedSportData) => {
   try {
-    const sport = await getMySportById(id);
-    if (sport) {
+    const sport = await getMySportById(id); //Verifico que el existencia del doc
+    const nameAlreadyExists = await checkSportExists(updatedSportData); //Verifico la existencia del nombre del deporte
+    if (sport && !nameAlreadyExists) {
+      //Si existe el doc y el nombre no esta en uso
       sport.set(updatedSportData);
       await sport.save();
       return sport;
+    } else if (nameAlreadyExists) {
+      throw new Error("Sport name already exists");
     }
   } catch (err) {
-    throw new Error("Error updating");
+    throw new Error("Error updating sport: " + err.message);
   }
 };
