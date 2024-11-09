7d29409ca5018c3dfde19e54926df0404de1cb4c
Miguel Mejia <00104923@uca.edu.sv>
2024-11-09 12:36:44 -0600

    [Proyecto] Add: Diseño inicial para la tabla de registro de partidos pasados en History

--- FrontEnd/src/components/Admin/Table/TableData.jsx
+++ FrontEnd/src/components/Admin/Table/TableData.jsx
@@ -1,200 +1,102 @@
-import React, { useState } from "react";
-import DataTable from "react-data-table-component";
-import { Modal, Button } from "react-bootstrap";
+import TableComponent, {createTheme} from "react-data-table-component";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./TableData.css";
 
 const TableData = () => {
-  //Este es un array de objetos, esta estático solamente para ver como se visualiza la información en la Tabla
-  const data = [
-    {
-      id: 1,
-      deporte: "Fútbol 11",
-      fecha: "15/09/2024",
-      hora: "10:00",
-      universidad_1: "UES",
-      universidad_2: "UCA",
-      estado: "No empezado",
-    },
-    {
-      id: 2,
-      deporte: "Baloncesto",
-      fecha: "18/09/2024",
-      hora: "14:00",
-      universidad_1: "UTEC",
-      universidad_2: "UDB",
-      estado: "En progreso",
-    },
-    {
-      id: 3,
-      deporte: "Voleibol",
-      fecha: "20/09/2024",
-      hora: "16:30",
-      universidad_1: "UFG",
-      universidad_2: "UEES",
-      estado: "Finalizado",
-    },
-    {
-      id: 4,
-      deporte: "Fútbol Sala",
-      fecha: "22/09/2024",
-      hora: "09:00",
-      universidad_1: "UNICAES",
-      universidad_2: "UNIVO",
-      estado: "No empezado",
-    },
-    {
-      id: 5,
-      deporte: "Baloncesto",
-      fecha: "25/09/2024",
-      hora: "11:00",
-      universidad_1: "UNAB",
-      universidad_2: "USAM",
-      estado: "En progreso",
-    },
-    {
-      id: 6,
-      deporte: "Voleibol",
-      fecha: "27/09/2024",
-      hora: "13:00",
-      universidad_1: "UJMD",
-      universidad_2: "UNASA",
-      estado: "Finalizado",
-    },
-    {
-      id: 7,
-      deporte: "Fútbol 11",
-      fecha: "30/09/2024",
-      hora: "15:00",
-      universidad_1: "UES",
-      universidad_2: "UDB",
-      estado: "No empezado",
-    },
-    {
-      id: 8,
-      deporte: "Fútbol Sala",
-      fecha: "02/10/2024",
-      hora: "17:00",
-      universidad_1: "UCA",
-      universidad_2: "UTEC",
-      estado: "En progreso",
-    },
-    {
-      id: 9,
-      deporte: "Baloncesto",
-      fecha: "05/10/2024",
-      hora: "19:00",
-      universidad_1: "UFG",
-      universidad_2: "UNIVO",
-      estado: "Finalizado",
-    },
-    {
-      id: 10,
-      deporte: "Voleibol",
-      fecha: "07/10/2024",
-      hora: "08:00",
-      universidad_1: "UNICAES",
-      universidad_2: "UEES",
-      estado: "No empezado",
-    },
-  ];
+    const columns = [
+        {name: "Deporte", selector: (row) => row.deporte, sortable: true,},
+        {name: "Universidad 1", selector: (row) => row.universidad_1},
+        {name: "Universidad 2", selector: (row) => row.universidad_2},
+        {name: "Marcador Universidad 1", selector: (row) => row.marcador_1},
+        {name: "Marcador Universidad 2", selector: (row) => row.marcador_2},
+        {name: "Ganador", selector: (row) => row.ganador},
+    ];
 
-  //Esta es una propiedad de la libreria de la Tabla, es para customizarla (estilos)
-  const customStyles = {
-    header: {
-      style: {
-        display: "none",
-      },
-    },
-    headCells: {
-      style: {
-        fontSize: "1.2rem",
-        fontFamily: '"Roboto", sans-serif',
-      },
-    },
+    const data = [
+        {
+            id: 1,
+            deporte: "Fútbol 11",
+            universidad_1: "UCA",
+            universidad_2: "UES",
+            marcador_1: 2,
+            marcador_2: 1,
+            ganador: "UCA",
+        },
+        {
+            id: 2,
+            deporte: "Baloncesto",
+            universidad_1: "UTEC",
+            universidad_2: "UNIVO",
+            marcador_1: 76,
+            marcador_2: 85,
+            ganador: "UNIVO",
+        },
+        {
+            id: 3,
+            deporte: "Voleibol",
+            universidad_1: "UFG",
+            universidad_2: "UDB",
+            marcador_1: 3,
+            marcador_2: 0,
+            ganador: "UFG",
+        },
+        {
+            id: 4,
+            deporte: "Fútbol Sala",
+            universidad_1: "UNICAES",
+            universidad_2: "USAM",
+            marcador_1: 4,
+            marcador_2: 4,
+            ganador: "Empate",
+        },
+        {
+            id: 5,
+            deporte: "Baloncesto",
+            universidad_1: "UES",
+            universidad_2: "UJMD",
+            marcador_1: 68,
+            marcador_2: 72,
+            ganador: "UJMD",
+        },
+    ];
 
-    rows: {
-      style: {
-        fontSize: "1rem",
-      },
-    },
-  };
 
-  const [showModal, setShowModal] = useState(false); //Mostrar el modal
-  const [selectedRow, setSelectedRow] = useState(null); //Saber si se ha seleccionada una fila
+    const customStyles = {
+        headCells: {
+            style: {
+                fontFamily: '"Roboto", sans-serif',
+                fontWeight: '500',
+            },
+        },
 
-  const handleRowClick = (row) => {
-    setSelectedRow(row);
-    setShowModal(true);
-  };
+        rows: {
+            style: {
+                minHeight: '3.5rem',
+                fontFamily: '"Roboto", sans-serif',
+            },
+        },
+    };
 
-  const handleEdit = () => {
-    console.log("Editar:", selectedRow);
-    // Lógica para editar
-    setShowModal(false);
-  };
+    createTheme('theme', {
+        background: {
+            default: '#f8f6f3',
+        },
+    })
 
-  const handleDelete = () => {
-    console.log("Eliminar:", selectedRow);
-    // Lógica para eliminar
-    setShowModal(false);
-  };
 
-  const columns = [
-    { name: "Deporte", selector: (row) => row.deporte, sortable: true },
-    { name: "Fecha", selector: (row) => row.fecha, sortable: true },
-    { name: "Hora", selector: (row) => row.hora, sortable: true },
-    {
-      name: "Universidad_1",
-      selector: (row) => row.universidad_1,
-      sortable: true,
-    },
-    {
-      name: "Universidad_2",
-      selector: (row) => row.universidad_2,
-      sortable: true,
-    },
-    { name: "Estado", selector: (row) => row.estado, sortable: true },
-  ];
+    return (
+        <>
+            <TableComponent  //Los estilos customizados
+                customStyles={customStyles}
+                            columns={columns} //las columnas
+                            data={data} //el array de objetos al inicio
+                            pagination
+                            pointerOnHover
+                            highlightOnHover
+                theme={'theme'}
 
-  return (
-    <section className={"matches-container"}>
-      <div className={"add-btn-container"}>
-        <button className={"add-btn"} title={"Nuevo partido"}>
-          <i className="fa-solid fa-circle-plus add"></i>
-        </button>
-      </div>
-      <DataTable
-        title="Partidos planificados" //Titulo de la tabla - lo he ocultado
-        customStyles={customStyles} //Los estilos customizados
-        columns={columns} //las columnas
-        data={data} //el array de objetos al inicio
-        pagination
-        onRowClicked={handleRowClick} //acciones al momento que se seleccione una fila
-        pointerOnHover
-        highlightOnHover
-      />
-
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Acciones</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Seleccione una acción para {selectedRow?.nombre}:</p>
-          <Button
-            variant="primary"
-            onClick={handleEdit}
-            style={{ marginRight: "10px" }}
-          >
-            Editar
-          </Button>
-          <Button variant="danger" onClick={handleDelete}>
-            Eliminar
-          </Button>
-        </Modal.Body>
-      </Modal>
-    </section>
-  );
+            />
+        </>
+    );
 };
 
 export default TableData;
--- FrontEnd/src/pages/Admin/History/History.jsx
+++ FrontEnd/src/pages/Admin/History/History.jsx
@@ -1,69 +1,18 @@
-import HeaderAdmin from "../../../components/Admin/HeaderAdmin/HeaderAdmin.jsx";
 import "./History.css";
 import NavAdmin from "../../../components/Admin/NavAdmin/NavAdmin.jsx";
 import Header from "../../../components/Common/Header/Header.jsx";
-import React from "react";
+import Footer from "../../../components/Common/Footer/Footer.jsx";
+import TableData from "../../../components/Admin/Table/TableData.jsx";
 
 const History = () => {
-  const matches = [
-    {
-      teamA: "Equipo A",
-      scoreA: 3,
-      teamB: "Equipo B",
-      scoreB: 1,
-      date: "03/11/2024",
-      time: "14:00",
-    },
-    {
-      teamA: "Equipo C",
-      scoreA: 2,
-      teamB: "Equipo D",
-      scoreB: 2,
-      date: "02/11/2024",
-      time: "16:00",
-    },
-    {
-      teamA: "Equipo E",
-      scoreA: 1,
-      teamB: "Equipo F",
-      scoreB: 3,
-      date: "01/11/2024",
-      time: "10:00",
-    },
-  ];
-
   return (
-    <section className="history-container">
+    <section className="admin-history-container">
       <NavAdmin />
       <Header message={"Visualiza tus eventos deportivos finalizados"} />
-      <h2 className="history-title">Historial de Partidos</h2>
-      <div className="table-container">
-        <table className="history-table">
-          <thead>
-            <tr>
-              <th>Equipo A</th>
-              <th>Resultado</th>
-              <th>Equipo B</th>
-              <th>Fecha</th>
-              <th>Hora</th>
-            </tr>
-          </thead>
-          <tbody>
-            {matches.map((match, index) => (
-              <tr key={index}>
-                <td>{match.teamA}</td>
-                <td className="score">
-                  <span className="scoreA">{match.scoreA}</span> -{" "}
-                  <span className="scoreB">{match.scoreB}</span>
-                </td>
-                <td>{match.teamB}</td>
-                <td>{match.date}</td>
-                <td>{match.time}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <main className="admin-history-main-content">
+          <TableData />
+      </main>
+      <Footer />
     </section>
   );
 };
