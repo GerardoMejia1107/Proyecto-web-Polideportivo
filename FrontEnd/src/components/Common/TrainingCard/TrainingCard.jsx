831a228fffbb4c0bff28e5fcbdd892196fef32af
GerardoMejia1107 <00104923@uca.edu.sv>
2024-11-10 07:13:27 -0600

    [Proyecto] Add: Reparacion de las fuentes que no estaban siendo aplicadas, y tamaños de fuente en componentes del usuario

--- FrontEnd/src/components/Common/TrainingCard/TrainingCard.jsx
+++ FrontEnd/src/components/Common/TrainingCard/TrainingCard.jsx
@@ -9,22 +9,24 @@ const TrainingCard = ({ training }) => {
             <div className="training-place">Lugar: {training.place}</div>
             <div className={`training-status ${training.status.replace(" ", "-").toLowerCase()}`}>
                 {training.status === "No decidido" && (
-                    <>
+                    <div className={'btn-training-status-container'}>
                         <button role="img" aria-label="no-decidido" className="status-icon training-status-btn"><i
-                            className="fa-regular fa-face-meh"></i></button> No decidido
-                    </>
+                            className="fa-regular fa-face-meh"></i> <span className={'doubt'}>No decidido</span>
+                        </button>
+                    </div>
                 )}
                 {training.status === "Cancelado" && (
-                    <>
+                    <div className={'btn-training-status-container'}>
                         <button role="img" aria-label="cancelado" className="status-icon training-status-btn"><i
-                            className="fa-regular fa-face-frown-open"></i></button> Cancelado
-                    </>
+                            className="fa-regular fa-face-frown-open"></i> <span className={'cancel'}>Cancelado</span>
+                        </button>
+                    </div>
                 )}
                 {training.status === "Confirmado" && (
-                    <>
+                    <div className={'btn-training-status-container'}>
                         <button role="img" aria-label="confirmado" className="status-icon training-status-btn"><i
-                            className="fa-regular fa-face-smile"></i></button> Confirmado
-                    </>
+                            className="fa-regular fa-face-smile"></i> <span className={'confirm'}>Confirmado</span></button>
+                    </div>
                 )}
             </div>
         </div>
