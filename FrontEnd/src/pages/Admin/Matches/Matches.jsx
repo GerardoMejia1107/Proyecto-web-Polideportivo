3781b00cacff3844ed4fa469c4330e065a34de99
GerardoMejia1107 <00104923@uca.edu.sv>
2024-11-09 21:32:43 -0600

    [Proyecto] Add: Diseño inicial de la pagina de Matches para el administrador

--- FrontEnd/src/pages/Admin/Matches/Matches.jsx
+++ FrontEnd/src/pages/Admin/Matches/Matches.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
 import './Matches.css';
+import NavAdmin from "../../../components/Admin/NavAdmin/NavAdmin.jsx";
+import Header from "../../../components/Common/Header/Header.jsx";
+import Footer from "../../../components/Common/Footer/Footer.jsx";
+import TableData from "../../../components/Admin/Table/TableData.jsx";
 
 
 const Matches = () => {
     return (
-        <section className="matches-container">
-
-
+        <section className="admin-matches-page-container">
+            <NavAdmin/>
+            <Header message={'Crea y consulta los partidos de tu institución'}/>
+            <main className={'admin-matches-view-content'}>
+                <TableData/>
+            </main>
+            <Footer/>
         </section>
     );
 };
