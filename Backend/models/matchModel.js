17febfdd9dcf16ff12da67d574fe0263b4ebe962
Miguel Mejia <00104923@uca.edu.sv>
2024-11-20 15:54:10 -0600

    [Proyecto] Add: Campo de visibilidad, y estado para el model de matchModel.js, servirá para saber si el partido esta visible y que pueda ser renderizado en la pagina del usuario corriente, y determinar el estado (planificado, cancelado, finalizado, en curso)

--- Backend/models/matchModel.js
+++ Backend/models/matchModel.js
@@ -1,34 +1,44 @@
 import mongoose from "mongoose";
 
 const matchSchema = new mongoose.Schema({
-  
-  id_first_univ: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "University",
-    required: true,
-  },
 
-  id_second_univ: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "University",
-    required: true,
-  },
+    id_first_univ: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "University",
+        required: true,
+    },
 
-  id_sport:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Sport",
-    required: true,
-  },
+    id_second_univ: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "University",
+        required: true,
+    },
 
-  date: {
-    type: Date,
-    required: true,
-  },
+    id_sport: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Sport",
+        required: true,
+    },
 
-  location: {
-    type: String,
-    required: true,
-  },
+    date: {
+        type: Date,
+        required: true,
+    },
+
+    location: {
+        type: String,
+        required: true,
+    },
+
+    status: {
+        type: String,
+        required: true,
+    },
+
+    visibility: {
+        type: Boolean,
+
+    }
 });
 
 const Match = mongoose.model("Match", matchSchema);
